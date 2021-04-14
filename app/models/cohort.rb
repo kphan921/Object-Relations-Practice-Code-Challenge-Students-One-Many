a8a9a036b8111a5e1a362684857c762a58047c40
Tri <triphan921@gmail.com>
2021-04-14 11:12:16 -0700

    commit tree 7a9d303678235c0dcc90dd70bc1f319c1a4e6d61

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,24 @@
 class Cohort < ActiveRecord::Base
+    has_many :students
+
+    def add_student(name, age)
+        Student.create(name: name, age: age, email: Faker::Internet.email, cohort_id: self.id)
+    end
+
+    def average_age
+        self.students.sum(:age).to_f / self.students.size
+    end
+
+    def total_students
+        self.students.size
+    end
+
+    def self.biggest
+        self.all.sort_by{|c| c.students.size}.last
+    end
+
+    def self.sort_by_mod
+        self.all.sort_by{|c| c.current_mod}
+    end
   
 end
